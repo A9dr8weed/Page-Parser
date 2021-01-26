055df3ab6eb982a344944ed720ad69ee944e987d
Andrew <andsof123455@gmail.com>
2021-01-26 13:56:26 +0200

    Add env, pars with using lxml

--- Page Parser/Page_Parser.py
+++ Page Parser/Page_Parser.py
@@ -1,36 +1,31 @@
-import csv
-from lxml import etree
-import lxml.html
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 def get_html(url):
-    response = requests.get(url)
+    r = requests.get(url)
+    r.encoding = 'utf8'
 
-    return response.text
+    return r.text
 
-def parse(api, url):
-    tree = lxml.html.document_fromstring(api)
-    railBioInfoItem = tree.xpath('//*[@id="two_column-4"]/div//*[@class="railBioInfoItem__label"]/text()')
-    railBioLinks = tree.xpath('//*[@id="two_column-4"]/div//*[@class="railBioLinks"]/li/text()')
-        
-    title = tree.findtext('.//title')
+def csv_read(data):
+    with open("C://Users//Andrew//Desktop//data.csv", 'w', newline = '') as file:
+        writer = csv.writer(file)
+        writer.writerow((data['head'], data['link']))
 
-    for rail in range(len(railBioInfoItem)):
-        data = {"railBioInfoItem": railBioInfoItem, "railBioLinks": railBioLinks, "title": title, "url": url}
-        save(data)
-
-def save(data):
-    with open("C:\\Users\\Andrew\\Desktop\\text.csv", "w", newline = '') as csv_file:
-        write = csv.writer(csv_file, delimiter = " ")
-        write.writerow("Name: " + data["title"])
-        write.writerow(data["url"])
-        write.writerow(data["railBioInfoItem"])
-        write.writerow(data["railBioLinks"])
+def get_link(html):
+    soup = BeautifulSoup(html, 'lxml')
+    head = soup.find('section', class_='content content_type_catalog').find_all('a', class_="goods-tile__heading")
+    for i in head:
+        link = i.get('href')
+        heads = i.find('span').get_text()
+        data = {'head': heads,
+                 'link': link}
+        print(data)
+        csv_read(data)
 
 def main():
-    url = 'https://www.marvel.com/characters/3-d-man-chandler'
-    parse(get_html(url), url)
+    data = get_link(get_html('https://rozetka.com.ua/ua/notebooks/c80004/'))
 
 if __name__ == "__main__":
-    main()	
+    main()
